e12b0603963362bb01a24a2cdc6fefadabae7dff
Ding128 <qwseda123@gmail.com>
2020-03-04 21:18:15 -0800

    Create Visit Map service. Closes #46

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtyService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtyService.java
@@ -3,6 +3,4 @@ package guru.springframework.sfgpetclinic.services;
 import guru.springframework.sfgpetclinic.model.Specialty;
 
 public interface SpecialtyService extends CrudService<Specialty, Long> {
-
-
 }
