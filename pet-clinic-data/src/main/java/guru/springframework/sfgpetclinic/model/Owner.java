98df4c3b3bf97d9389afa8f501a92afaad6a0d1a
Ding128 <qwseda123@gmail.com>
2020-03-07 14:52:01 -0800

    Write CRUD test for Owner Map Service. Closes #49

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -12,18 +12,22 @@ import java.util.Set;
 @Setter
 @Getter
 @NoArgsConstructor
-@Builder
 @Entity
 @Table(name = "owners")
 public class Owner  extends Person{
+
     @Builder
-    public Owner(String address, String city, String telephone, Set<Pet> pets) {
+    public Owner(Long id, String firstName, String lastName, String address, String city,
+                 String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
         this.address = address;
         this.city = city;
         this.telephone = telephone;
-        this.pets = pets;
-    }
 
+        if(pets != null) {
+            this.pets = pets;
+        }
+    }
     @Column(name = "address")
     private String address;
 
